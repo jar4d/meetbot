f8ba228f2ac523fa15a4b8605bd6457d66e41fb0
jar4d <jared.ruddy@gmail.com>
2018-01-04 22:03:11 +0000

    3

--- server.js
+++ server.js
@@ -52,10 +52,51 @@ app.get('/*', function(req, res) {
     //var databaseSearch = Places.find();
 
     var jsonResponse = [];
-    jsonResponse.push({ "text": "https://www.google.com/maps/dir/" + data.latitude + "," + data.longitude + "/48.179926,27.28875860"});
+    //jsonResponse.push({ "text": "https://www.google.com/maps/dir/" + data.latitude + "," + data.longitude + "/48.179926,27.28875860"});
+    jsonResponse.push(
+        {
+     "messages": [
+        {
+          "attachment":{
+            "type":"template",
+            "payload":{
+              "template_type":"list",
+              "top_element_style":"large",
+              "elements":[
+                {
+                  "title":"Chatfuel Rockets T-Shirt",
+                  "image_url":"http://rockets.chatfuel.com/img/shirt.png",
+                  "subtitle":"Soft white cotton t-shirt with CF Rockets logo",
+                  "buttons":[
+                    {
+                      "type":"web_url",
+                      "url":"https://rockets.chatfuel.com/store/shirt",
+                      "title":"View Item"
+                    }
+                  ]
+                },
+                {
+                  "title":"Chatfuel Rockets Hoodie",
+                  "image_url":"http://rockets.chatfuel.com/img/hoodie.png",
+                  "subtitle":"Soft gray cotton t-shirt with CF Rockets logo",
+                  "buttons":[
+                    {
+                      "type":"web_url",
+                      "url":"https://rockets.chatfuel.com/store/hoodie",
+                      "title":"View Item"
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        }
+      ]
+    }
+    );
+    
     res.send(jsonResponse);
 
-    //https://www.google.com/maps/dir/{{latitude}},{{longitude}}/48.179926,27.28875860
 });
 
 
