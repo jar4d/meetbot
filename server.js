392a815ae6c397978f0b3b0a774192b30d1bb917
jar4d <jared.ruddy@gmail.com>
2018-01-06 15:02:02 +0000

    a

--- server.js
+++ server.js
@@ -76,7 +76,46 @@ app.get('/*', function(req, res) {
       }
     }
 
-
+    {
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"generic",
+          "image_aspect_ratio": "square",
+          "elements":[
+            {
+              "title":"Chatfuel Rockets T-Shirt",
+              "image_url":"https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/275px-A_small_cup_of_coffee.JPG",
+              "subtitle":"Soft white cotton t-shirt with CF Rockets logo",
+              "buttons":[
+                {
+                  "type":"web_url",
+                  "url":"https://rockets.chatfuel.com/store/shirt",
+                  "title":"View Item"
+                }
+              ]
+            },
+            {
+              "title":"Chatfuel Rockets Hoodie",
+              "image_url":"https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/275px-A_small_cup_of_coffee.JPG",
+              "subtitle":"Soft grey cotton hoddie with CF Rockets logo",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://rockets.chatfuel.com/store",
+                "messenger_extensions": true
+              },
+              "buttons":[
+                {
+                  "type":"web_url",
+                  "url":"https://rockets.chatfuel.com/store/hoodie",
+                  "title":"View Item"
+                }
+              ]
+            }
+          ]
+        }
+      }
+    }
 
     );
     res.send(jsonResponse);
