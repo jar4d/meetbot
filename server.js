bb48c6171c71dd8c6aab651cf694fb29a8003956
jar4d <jared.ruddy@gmail.com>
2018-01-06 15:02:46 +0000

    a

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/*', function(req, res) {
           "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/275px-A_small_cup_of_coffee.JPG"
         }
       }
-    }
+    },
 
     {
       "attachment":{
