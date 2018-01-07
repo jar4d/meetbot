6ec267e7216badcb105dda86e457e1e5923c06ef
jar4d <jared.ruddy@gmail.com>
2018-01-07 16:09:01 +0000

    a

--- server.js
+++ server.js
@@ -28,12 +28,10 @@ app.get('/*', function(req, res) {
     console.log('latitude: ', data.latitude);  
 
     //look at DB
-    var cursor = db.collection("locations").find();
-    var count = db.collection("locations").find().count();
-    db.collection('locations').find().toArray(function(err, results) {
-      console.log("DB count: " + count);
+    //var cursor = db.locations.find();
+    var count = db.locations.find().count();
+    console.log("DB count: " + count);
       // send HTML file populated with quotes here
-    });    
 
     var jsonResponse = [];
 
