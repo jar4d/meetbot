cb926d8ee8cd5f70831b266569369385f8db3244
jar4d <jared.ruddy@gmail.com>
2018-01-07 21:36:13 +0000

    a

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var url = 'mongodb://meetbotuser:meetbot@ds247077.mlab.com:47077/meetbot';
 
 MongoClient.connect(url)
     .then(function(db){
+    const database = db.db('meetbot');
+    database.collection('locations');        
     var count = database.collection('meetbot').find().count();
     console.log("DB count: " + count);        
 
