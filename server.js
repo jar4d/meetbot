3679dfd39ec9c00eb924471d6e4bb03c4b9e101a
jar4d <jared.ruddy@gmail.com>
2018-01-07 22:22:49 +0000

    a

--- server.js
+++ server.js
@@ -1,25 +1,130 @@
 
 var express = require('express');
 var app = express();
-
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://meetbotuser:meetbot@ds247077.mlab.com:47077/meetbot';
 
 
-MongoClient.connect(url)
-    .then(function(db){
-    const database = db.db('meetbot');
-    return database.collection('locations');      
-    })
-    .then(function(locations){
-    var count = database.collection('locations').find().count();
-    console.log("DB count: " + count);      
-    })
-    
-.catch(function(err){
-    console.log(err)
-    })
+    MongoClient.connect('mongodb://meetbotuser:meetbot@ds247077.mlab.com:47077/meetbot', function (err, client) {
+        app.listen(80, () => {
+        console.log('Chatfuel Bot-Server listening on port 80...')
+        });
+
+        if (err) throw err;
+        var db = client.db('meetbot');
+
+        db.collection('locations').findOne({}, function (findErr, result) {
+            if (findErr) throw findErr;
+            console.log(result);
+            client.close();
+        });
+
+
+        app.get('/*', function(req, res) {
+
+            //get stuff from API push
+            var data = req.query; 
+            //console.log('REQ Item: ', p);   // shows all data...
+            console.log('New query...'); 
+            console.log('venueType: ', data.venueType);   
+            console.log('longitude: ', data.longitude);   
+            console.log('latitude: ', data.latitude);  
+
+
+            cursor = database.collection('meetbot').find();
+            var count = database.collection('meetbot').find().count();
+            countstring = JSON.stringify( count );
+            console.log("DB count: " + count);
+
+            var jsonResponse = [];
+
+            jsonResponse.push(
+
+           {"text": "How about The Smoking Goat?"},   
+           {"text": "It's a Thai Barbecue Bar."},
+
+          {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"generic",
+                "elements":[
+                   
+                   {
+                    "title":"LALALA0",
+                    "image_url":"https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/26151403_788886704629335_7818346908434300928_n.jpg",
+                    "subtitle":"Weve got the right hat for everyone.",
+                    /*
+                    "default_action": {
+                      "type": "web_url",
+                      "url": "http://smokinggoatbar.com/shoreditch/",
+                      "messenger_extensions": true,
+                      "webview_height_ratio": "full",
+                      "fallback_url": "http://smokinggoatbar.com/shoreditch/"
+                    },
+                    */
+                    "buttons":[
+                      {
+                        "type":"web_url",
+                        "url":"http://smokinggoatbar.com/shoreditch/",
+                        "title":"Location"
+                      },
+                      {
+                        "type":"web_url",
+                        "url":"http://smokinggoatbar.com/shoreditch/",
+                        "title":"Share"
+                      }        
+                    ]      
+                  },
+
+                   {
+                    "title":"LALALA1",
+                    "image_url":"https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/26151403_788886704629335_7818346908434300928_n.jpg",
+                    "subtitle":"Weve got the right hat for everyone.",
+                    
+                    "buttons":[
+                      {
+                        "type":"web_url",
+                        "url":"http://smokinggoatbar.com/shoreditch/",
+                        "title":"Location"
+                      },
+                      {
+                        "type":"web_url",
+                        "url":"http://smokinggoatbar.com/shoreditch/",
+                        "title":"Share"
+                      }        
+                    ]      
+                  },
+
+                   {
+                    "title":"LALALA2",
+                    "image_url":"https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/26151403_788886704629335_7818346908434300928_n.jpg",
+                    "subtitle":"Weve got the right hat for everyone.",
+                    
+                    "buttons":[
+                      {
+                        "type":"web_url",
+                        "url":"http://smokinggoatbar.com/shoreditch/",
+                        "title":"Location"
+                      },
+                      {
+                        "type":"web_url",
+                        "url":"http://smokinggoatbar.com/shoreditch/",
+                        "title":"Share"
+                      }        
+                    ]      
+                  },
 
 
 
+                ]
+              }
+            }
+          }     
+             
+            );
 
+
+            res.send(jsonResponse);
+        });
+
+}); 
