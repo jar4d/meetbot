51a02a8a8e73f21f096b9f2570eaf04d0b521308
jar4d <jared.ruddy@gmail.com>
2018-01-09 15:25:25 +0000

    a

--- server2.js
+++ server2.js
@@ -6,7 +6,7 @@ var locationscount = [];
 var locationsmatched = [];
 var url = 'mongodb://meetbotuser:meetbot@ds247077.mlab.com:47077/meetbot';
 var JsonBody
-
+var JsonElements
 
 app.listen(80, () => {
 console.log('Chatfuel Bot-Server listening on port 80...')
@@ -21,63 +21,71 @@ MongoClient.connect(url, function (err, client) {
             if (err) throw err;
             console.log("locationsmatched: "+ locationsmatched);
 
+            db.collection('locations').find({}).count({}, function (err, result) { //{drink:data.drink, vibe:data.vibe} db.collection.find( { field: { $gt: value1, $lt: value2 } } );
+                var locationscount = result;
+                if (err) throw err;
+                console.log("locationscount: "+ locationscount);
+                client.close();
+
+                    app.get('/*', function(req, res) {
+                        //get stuff from API push
+                        var data = req.query; 
+                        console.log("locationscount outside APP GET: " + locationscount);
+                        console.log("locationsmatched outside APP GET: " + locationsmatched);
+
+                        var elementsArray = [];
+                        var jsonResponse = [];
+                        //initial result response
+                        //jsonResponse.push(
+                        //    {"text": "Here are our picks for "+ data.vibe + " " + data.drink + " places less than 1/4 mile walk away."}
+                        //);
+
+                        //container for gallery result elements
+                        jsonResponse.push(
+                            var JsonBody                
+                        );
+
+                        //iterate over results...
+                        //for(var i = 0; i < locationscount; ++i) {
+                        //console.log("looping  " + [i]);
+
+                        elementsArray.push(
+                            var JsonElements
+                        );
+
+                        jsonResponse[0].attachment.payload.elements.push(elementsArray[0]);
+                        jsonResponsestringify = JSON.stringify(jsonResponse);
+                        res.send(jsonResponse); //not string
+                        console.log("Sent jsonResponse: " + jsonResponsestringify);         
+
+                    });
+
+
+            });
 
         });
-    client.close();
 });
 
 
 
 
-        app.get('/*', function(req, res) {
 
 
-            //get stuff from API push
-            var data = req.query; 
-            //console.log('REQ Item: ', p);   // shows all data...
-            console.log('New query...'); 
-            console.log('Vibe: ', data.vibe);   
-            console.log('Drink: ', data.drink);       
-            console.log('longitude: ', data.longitude);   
-            console.log('latitude: ', data.latitude);  
-        
 
 
-            if (err) throw err;
-            var db = client.db('meetbot');
+
+var JsonBody =      {
+                    attachment:{
+                        type:"template",
+                        payload:{
+                            template_type:"generic",
+                            elements:[]
+                        }
+                    }
+                }   
 
 
-            db.collection('locations').find({}).count({}, function (findErr, result) { //{drink:data.drink, vibe:data.vibe} db.collection.find( { field: { $gt: value1, $lt: value2 } } );
-                if (findErr) throw findErr;
-                var locationscount = result;
-                console.log("locationscount: "+ locationscount);
-                client.close();
-            });
-
-   
-
-            console.log("locationscount outside: " + locationscount);
-            console.log("locationsmatched outside: " + locationsmatched);
-
-            var elementsArray = [];
-            var jsonResponse = [];
-            //initial result response
-            //jsonResponse.push(
-            //    {"text": "Here are our picks for "+ data.vibe + " " + data.drink + " places less than 1/4 mile walk away."}
-            //);
-
-            //container for gallery result elements
-            jsonResponse.push(
-                JsonBody                
-            );
-
-            //iterate over results...
-            //for(var i = 0; i < locationscount; ++i) {
-            //console.log("looping  " + [i]);
-
-            elementsArray.push(
-                //########start of element#########
-                {                  
+var JsonElements = {                  
                     title:locationsmatched.name,
                     image_url:locationsmatched.imageURL,
                     subtitle:locationsmatched.description, 
@@ -95,30 +103,4 @@ MongoClient.connect(url, function (err, client) {
                         }        
                     ]      
 
-                }
-                //########end of element#########
-                );
-
-            jsonResponse[0].attachment.payload.elements.push(elementsArray[0]);
-
-            jsonResponsestringify = JSON.stringify(jsonResponse);
-            res.send(jsonResponse); //not string
-            console.log("Sent jsonResponse: " + jsonResponsestringify);         
-
-        });
-
-
-
-
-JsonBody =                 {
-                    attachment:{
-                        type:"template",
-                        payload:{
-                            template_type:"generic",
-                            elements:[]
-                        }
-                    }
-                }   
-
-
-
+                },
