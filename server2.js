71f7d5f42c6cbb9f659ba42b59e452b5f8acd2ca
jar4d <jared.ruddy@gmail.com>
2018-01-29 14:19:14 +0000

    a

--- server2.js
+++ server2.js
@@ -123,8 +123,10 @@ app.get('/*', function(req, res) {
 
                                 //need to limit this to 3...
 
-                                jsonResponse[0].attachment.payload.elements.push(elementsArray[i]);
-                                if(i<4){i = i+1;}
+                                if(i<4){
+                                    jsonResponse[0].attachment.payload.elements.push(elementsArray[i]);
+                                    i = i+1;
+                                }else{}
                             }
                           });
                     });
